38386dd1f1dd242b6efd7c3789bc50de61c3b7ad
double-hash <33376825+double-hash@users.noreply.github.com>
2020-10-26 09:37:28 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,31 @@
+import subprocess
+import shlex
+
+devices = {}
+
+# parse each lines
+def parse_output(output):
+          # split output line
+          words = output.split()
+          # check if RSSI is present
+          if len(words) == 6 and words[4] == "RSSI:":
+                 # words[2] is always MAC addr
+                 devices[words[3]] = words[5]
+
+def run_command(command):
+    #opening process
+    process = subprocess.Popen(shlex.split(command), shell=True, stdout=sub$
+    #looping for getting new values
+    while True:
+         output = process.stdout.readline()
+         if process.poll() is not None:
+             break
+         if output:
+	     #formatting output and storing in devices
+             parse_output(output)
+             #print output stored in devices
+             print devices
+    rc = process.poll()
+    return rc
+
+run_command('bluetoothctl scan on')
