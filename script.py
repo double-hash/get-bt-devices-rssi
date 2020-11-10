c8e58e38d063714678ae9171a1367702c904d0ca
double-hash <33376825+double-hash@users.noreply.github.com>
2020-11-10 15:57:26 +0100

    script fix + comments added

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+  GNU nano 3.2                                               test2.py                                                        
+
 import subprocess
 import shlex
 
@@ -5,28 +7,36 @@ devices = {}
 
 # parse each lines
 def parse_output(output):
-          # split output line
-          words = output.split()
-          # check if RSSI is present
-          if len(words) == 6 and words[4] == "RSSI:":
-                 # words[3] is always MAC addr
-		 # sorting MAC addr as key and RSSI as value in devices
-                 devices[words[3]] = words[5]
+        # split output line
+        words = output.split()
+        # print(words)
+        # check if RSSI is present
+        if len(words) == 5 and words[3] == "RSSI:":
+                i = words.index("RSSI:")
+                if i:
+                        # cleaning the RSSI value and storing as an int value
+                        s = words[4].replace('-','')
+                        # words[2] is always MAC addr
+                        devices[words[2]] = words[4]
 
 def run_command(command):
-    #opening process
-    process = subprocess.Popen(shlex.split(command), shell=True, stdout=subprocess.PIPE)
-    #looping for getting new values
-    while True:
-         output = process.stdout.readline()
-         if process.poll() is not None:
-             break
-         if output:
-	     #formatting output and storing in devices
-             parse_output(output)
-             #print output stored in devices
-             print devices
-    rc = process.poll()
-    return rc
+        process = subprocess.Popen(shlex.split(command), shell=True, stdout=subprocess.PIPE)
+        while True:
+                output = process.stdout.readline()
+                if process.poll() is not None:
+                        break
+                if output:
+                        #print(output)
+                        parse_output(output)
+                        print(devices)
+        rc = process.poll()
+        return rc
 
 run_command('bluetoothctl scan on')
+#if the command doesn't start automatically, just type 'scan on' after launching the python script
+
+
+
+
+
+
